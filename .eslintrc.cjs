653a1e91014ef96523ad43e4ac9c97514752707a
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2023-03-14 16:02:28 +0100

    feat: enhanced Profile

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ module.exports = {
         extraFileExtensions: [".astro"],
       },
       rules: {
+        "react/jsx-key": ["off"],
         "react/self-closing-comp": ["off"],
       },
     },
